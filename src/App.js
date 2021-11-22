ec10dd90e41f0900d9fc1a5804d7346fe2a252ac
BibhushanKarki <bibhushan.karki11@gmail.com>
2021-11-22 20:57:10 +0545

    routes and css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import React from "react";
-import {Switch,route,Link} from "react-router-dom";
+import {Switch,Route,Link} from "react-router-dom";
 import {Layout,Typography,Space} from "antd";
+import {Navbar,Exchanges,Homepage,Cryptocurrencies,News,CryptoDetails} from "./components";
+import './App.css'
 
 const App = () => {
   return (
-    <div className="App">
+    <div className="app">
       <div className="navbar">
-
+        <Navbar />
       </div>
       <div className="main">
-
+        <Layout>
+          <div className="routes">
+            <Switch>
+            <Route exact path="/">
+                <Homepage />
+              </Route>
+              <Route exact path="/exchanges">
+                <Exchanges />
+              </Route>
+              <Route exact path="/cryptocurrencies">
+                <Cryptocurrencies />
+              </Route>
+              <Route exact path="/crypto/:coinId">
+                <CryptoDetails />
+              </Route>
+              <Route exact path="/news">
+                <News />
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
       </div>
       <div className="footer">
-        
+
       </div>
     </div>
   );
